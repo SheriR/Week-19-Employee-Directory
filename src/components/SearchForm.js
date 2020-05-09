24c62a65cb6aba0c0d046b4289a13e243364c5af
Sheri Rhoades <sheri_rhoades@yahoo.com>
2020-05-09 13:59:05 -0500

    heroku

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -16,23 +16,4 @@ function SearchForm(props) {
   );
 }
 
-//     <div class="input-group mb-3">
-//       <div class="input-group-prepend">
-//         <span class="input-group-text" id="basic-addon1">
-//           Search
-//         </span>
-//       </div>
-//       <input
-//         onChange={(event) => props.startSort(event)}
-//         name="search"
-//         type="text"
-//         class="form-control"
-//         placeholder="Search for an employee"
-//         aria-label="Username"
-//         aria-describedby="basic-addon1"
-//       />
-//     </div>
-//   );
-// }
-
 export default SearchForm;
